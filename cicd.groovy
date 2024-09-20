d99653b78c762ba253fa0ee6af6c0f6c5e67cbd4
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:35 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/mesonport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/mesonport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/mesonport.git'), string(name: 'PORT_DESCRIPTION', value: 'Meson is a project to create the best possible next-generation build system.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/mesonport.git'), string(name: 'PORT_DESCRIPTION', value: 'Meson is a project to create the best possible next-generation build system.' )]
   }
 }
